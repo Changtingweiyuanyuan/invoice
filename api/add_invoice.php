c7f9a5215925c34f8a4cbd3736d3f39b4db30d52
tingwei <yuan097@kimo.com>
2020-11-12 15:58:33 +0800

    完成發票新增功能及資料表建置

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -3,4 +3,46 @@
 //將發票的號碼及相關資訊寫入資料庫
 
 
+$dsn="mysql:host=localhost;dbname=invoice;chrset=utf8";
+$pdo=new PDO($dsn,'root','');
+
+
+// foreach($_POST as $key => $value){
+//     echo "欄位".$key."==值".$value;
+// }
+// 之前作法 $code=$_POST['code'];
+// $sql="insert into invoice () values('{$_POST['code']}','{}','{}','{}','{}','{}')";
+
+foreach($_POST as $key => $value){
+    $tmp[]=$key;
+}
+
+// implode已經是把陣列value取出來了 不需要再執行下面的foreach
+// foreach($_POST as $key => $value){
+//     $tmp2[]=$value;
+// }
+
+echo "<pre>";
+print_r(array_keys($_POST));
+echo "</pre>";
+echo '<br>';
+// echo "<pre>";
+// print_r($tmp);
+// echo "</pre>";
+
+// $tmp可以換成array_keys($_POST)
+echo 'insert into invoices(`'.implode('`,`',array_keys($_POST)).'`)';
+
+// tmp2直接換成$_POST
+echo "values('".implode("','",$_POST)."')";
+echo '<br>';
+$sql="insert into invoices(`".implode('`,`',array_keys($_POST))."`) values('".implode("','",$_POST)."')";
+echo $sql;
+$pdo->exec($sql);
+
+echo "新增完成";
+header("location:../index.php");
+
+
+
 ?>
--- main.php
+++ main.php
@@ -30,8 +30,8 @@ $m=ceil(date("m")/2);
 <div class="border d-flex justify-content-center p-2">
 
 <form action="api/add_invoice.php" method="post">
-    <div class="p-2">年份:<input type="text" name="year"></div>
-    <div class="p-2">期別:<select name="" id="">
+    <div class="p-2">日期:<input type="date" name="date"></div>
+    <div class="p-2">期別:<select name="period">
         <option value="1">1,2月</option>
         <option value="2">3,4月</option>
         <option value="3">5,6月</option>
@@ -40,8 +40,8 @@ $m=ceil(date("m")/2);
         <option value="6">11,12月</option>
     </select></div>
     <div class="p-2">
-    發票號碼:<input type="text" name="prepend">
-    <input type="text" name="number"></div>
+    發票號碼:<input type="text" name="code">
+    <input type="number" name="number"></div>
     <div class="p-2">發票金額:<input type="number" name="payment"></div>
     <div class="p-2 text-center"><input type="submit" value="送出囉"></div>
 </form>
