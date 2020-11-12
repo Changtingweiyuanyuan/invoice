a7768e7b5b4d3e9d297be31bf41e8d08257c459a
tingwei <yuan097@kimo.com>
2020-11-12 14:40:13 +0800

    完成首頁的畫面設計

--- index.php
+++ index.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>統一發票紀錄及對獎系統</title>
 </head>
 <body>
+
+
+<!-- 用include來寫主要程式,<head><body>不用一直重新寫 -->
+<!-- 但要寫:什麼時候該引入對的畫面 -->
+<?php
+if(isset($_GET['do'])){
+    $file=$_GET['do'].".php";
+    // file="invoice.php"
+    include $file;
+    // include invoice.php;
+}else{
+include "main.php";
+}
+?>
+
 
 </body>
 </html>
---
+++ main.php
@@ -0,0 +1,51 @@
+
+
+<h3 class="text-center p-3">統一發票紀錄</h3>
+
+
+
+<nav class="container col-8">
+<ul class="nav justify-content-around nav-tabs border p-3">
+
+<!-- 建立陣列 -->
+<?php
+    $month=[
+        1=>'1,2月',
+        2=>'3,4月',
+        3=>'5,6月',
+        4=>'7,8月',
+        5=>'9,10月',
+        6=>'11,12月'
+    ];
+$m=ceil(date("m")/2);
+
+?>
+    <il class="nav-item"><?=$month[$m];?></il>
+    <il class="nav-item"><a href="#">當期發票</a></il>
+    <il class="nav-item"><a href="#">兌獎</a></il>
+    <il class="nav-item"><a href="#">輸入獎號</a></il>
+</ul>
+
+
+<div class="border d-flex justify-content-center p-2">
+
+<form action="api/add_invoice.php" method="post">
+    <div class="p-2">年份:<input type="text" name="year"></div>
+    <div class="p-2">期別:<select name="" id="">
+        <option value="1">1,2月</option>
+        <option value="2">3,4月</option>
+        <option value="3">5,6月</option>
+        <option value="4">7,8月</option>
+        <option value="5">9,10月</option>
+        <option value="6">11,12月</option>
+    </select></div>
+    <div class="p-2">
+    發票號碼:<input type="text" name="prepend">
+    <input type="text" name="number"></div>
+    <div class="p-2">發票金額:<input type="number" name="payment"></div>
+    <div class="p-2 text-center"><input type="submit" value="送出囉"></div>
+</form>
+
+</div>
+
+</nav>
