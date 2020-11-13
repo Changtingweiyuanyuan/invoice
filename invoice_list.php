24aae26f16a00bc9fdd97dcdd2b757721151b27c
tingwei <yuan097@kimo.com>
2020-11-13 10:27:32 +0800

    增加一個用來顯示編輯資料用的表單，並透過編輯按鈕帶出資料的id值

--- invoice_list.php
+++ invoice_list.php
@@ -6,26 +6,28 @@ $sql="select * from `invoices` order by date desc";
 $rows=$pdo->query($sql)->fetchAll();
 ?>
 
-<table>
-    <tr class="table text-center">
+<table class="table text-center">
+    <tr>
         <td>發票號碼</td>
         <td>消費日期</td>
         <td>消費金額</td>
         <td>操作</td>
     </tr>
     <?php
+    // 把rows(二維陣列)拆成row(一維陣列) 再去呼叫裡面的value
     foreach($rows as $row){
     ?>
-    <tr class="table text-center">
+    <tr>
         <td><?=$row['code'].$row['number'];?></td>
         <td><?=$row['date'];?></td>
         <td><?=$row['payment'];?></td>
         <td>
-            <button class="btn btn-sm btn-primary">編輯</button>
-            <button class="btn btn-sm btn-danger">刪除</button>
+            <button class="btn btn-sm btn-light"><a class="text-dark" href="?do=edit_invoice&id=<?=$row['id'];?>">編輯</a></button>
+            <button class="btn btn-sm alert-danger">刪除</button>
         </td>
     </tr>
     <?php
     }
     ?>
 </table>
+
