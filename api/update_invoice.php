4ee5cb4463f22a178b1b3d91eba89bc33a385bdd
tingwei <yuan097@kimo.com>
2020-11-13 11:04:46 +0800

    完成編輯發票資料功能

---
+++ api/update_invoice.php
@@ -0,0 +1,26 @@
+<?php
+include_once "../base.php";
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+$sql="update
+        `invoices`
+    set
+        `code`='{$_POST['code']}',
+        `number`='{$_POST['number']}',
+        `date`='{$_POST['date']}',
+        `payment`='{$_POST['payment']}' 
+    where
+        `id`='{$_POST['id']}'";
+
+// exec更新資料不需要回傳資料 如果要回傳可以用query
+$pdo->exec($sql);
+
+if($pdo->exec($sql)==0){
+    header("location:../index.php?do=invoice_list");
+}else{echo '更新失敗';}
+// header("location:../index.php?do=invoice_list");
+
+?>
--- edit_invoice.php
+++ edit_invoice.php
@@ -3,7 +3,21 @@ include_once "base.php";
 
 $sql="select * from `invoices` where `id`='{$_GET['id']}'";
 $inv=$pdo->query($sql)->fetch();
-echo "<pre>";
-print_r($inv);
-echo "</pre>";
+// echo "<pre>";
+// print_r($inv);
+// echo "</pre>";
 ?>
+<!-- 傳值方式 1:用get ?後面傳值 2:post只能用表單 所以按鈕後面放name -->
+<!-- 方法1:<form action="api/update_invoice.php?id=<?=$inv['id']?>" method="post"> -->
+<form action="api/update_invoice.php" method="post">
+    <!-- 方法2 -->
+    <input type="hidden" name="id" value="<?=$inv['id']?>" readonly>
+    <div>發票號碼:<input type="text" name="code" style="width:30px" value="<?=$inv['code']?>">
+    <input type="text" name="number" value="<?=$inv['number']?>"></div>
+    <div>消費日期:<input type="date" name="date" value="<?=$inv['date']?>"></div>
+    <div>消費金額:<input type="text" name="payment" value="<?=$inv['payment']?>"></div>
+    <div>
+    <input type="submit" value="送出">
+    <input type="reset" value="重設">
+    </div>
+</form>
