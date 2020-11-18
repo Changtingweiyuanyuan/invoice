c0a3aadb554d0753e721a74edc5e2b44cdbed14a
tingwei <yuan097@kimo.com>
2020-11-18 14:15:33 +0800

    完成對單張發票的對獎做法

--- award.php
+++ award.php
@@ -29,9 +29,49 @@ $period=ceil(explode('-',$date)[1]/2);
 $awards=$pdo->query("select * from `award_numbers` where `year`='$year' && `period`='$period'")->fetchALL();
 
 //echo出來 看有沒有找到
-echo "<pre>";
-print_r($awards);
-echo "</pre>";
+// echo "<pre>";
+// print_r($awards);
+// echo "</pre>";
+
+foreach($awards as $award){
+    switch($award['type']){
+        // 特別獎(8碼全中 發票號碼)
+        case 1:
+            // echo '號碼='.$number.'<br>';
+            // echo '特別獎='.$award['number'].'<br>';
+            if($award['number']==$number){echo '號碼為'.$number.'<br>';echo '中特別獎啦~';}else{echo '特別獎沒中獎<br>';}
+        break;
+        
+        // 特獎(8碼全中 發票號碼)
+        case 2:
+            if($award['number']==$number){echo '號碼為'.$number.'<br>';echo '中特獎啦~';}else{echo '特獎沒中獎<br>';}
+        break;
+        
+        // 頭獎-6獎
+        case 3:
+            // 從頭獎開始對到六獎
+            // for($i=0;$i<6;$i++){
+            //     $target=mb_substr($award['number'],$i,(8-$i),'utf8');
+            //     $mynumber=mb_substr($number['number'],$i,(8-$i),'utf8');
+            //     if($target==$mynumber){echo '號碼為'.$number.'<br>';echo "中{$awardStr[$i]}獎啦~";}else{echo "{$awardStr[$i]}獎沒中";}
+            // }
+
+            // 從六獎開始對到頭獎 如果六獎沒中 後面都不用對了
+            for($i=5;$i>=0;$i--){
+                $target=mb_substr($award['number'],$i,(8-$i),'utf8');
+                $mynumber=mb_substr($number,$i,(8-$i),'utf8');
+                if($target==$mynumber){echo '號碼為'.$number;echo "中{$awardStr[$i]}獎啦<br>";}else{break;}
+            }
+        break;
+        
+        // 增開六獎 後三碼一樣
+        case 4:
+            if($award['number']==mb_substr($number,5,3,'utf8')){echo '號碼為'.$number;echo "中了增開六獎<br>";}
+        break;
+    }
+}
+
+// echo substr($award['number'],$i,8-$i);
 
 // print_r(explode('-',$date)[1]);
 
--- base.php
+++ base.php
@@ -5,4 +5,8 @@ $pdo=new PDO($dsn,'root','');
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
+
+// 中獎號碼的陣列
+$awardStr=['頭','二','三','四','五','六'];
+
 ?>
